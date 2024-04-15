faedf91b6ff66cf983bd74c46f6f60891e8c7bab
Enamul Khan <91288466+EnamulKhan2525@users.noreply.github.com>
2024-04-16 02:48:27 +0600

    Add files via upload

---
+++ Phitron/Introduction to Programming Language/Module 04 Assignment 01/Assignment_5_Shopping.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if (a > 1000)
+    {
+        printf("I will buy Punjabi\n");
+        if (a >= 1500)
+        {
+            printf("I will buy new shoes\nAlisa will buy new shoes");
+        } 
+    }
+    else
+    {
+       printf("Bad luck!");
+    }
+    return 0;
+}
