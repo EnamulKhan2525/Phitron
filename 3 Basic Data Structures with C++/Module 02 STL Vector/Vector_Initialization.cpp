3a9c17c494b32c9b3b315199d4b24da4140290f3
Enamul Khan <91288466+EnamulKhan2525@users.noreply.github.com>
2025-08-05 03:35:29 +0600

    Add files via upload

---
+++ 3 Basic Data Structures with C++/Module 02 STL Vector/Vector_Initialization.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int>v;
+    // vector<int>v(10);
+    vector<int>v(10,25);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<<endl;
+    }
+    
+    cout<<v.size();
+    return 0;
+}
