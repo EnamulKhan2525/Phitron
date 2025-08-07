0e6713287ef6ab70d4db00d21c3c03a8b3cef5df
Enamul Khan <91288466+EnamulKhan2525@users.noreply.github.com>
2025-08-07 10:24:51 +0600

    Add files via upload

---
+++ 3 Basic Data Structures with C++/Module 06 Singly Linked List Operations/Refference_Of_A_Pointer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void fun(int* &p)
+{
+    int y=200;
+    p=&y;
+    cout<<"fun "<< *p<<endl;
+}
+int main()
+{
+    int x =10;
+    int *p=&x;
+    fun(p);
+    cout<<"main "<< *p<<endl;
+    return 0;
+}
